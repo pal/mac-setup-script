1182d5a7e3609f2bfa7ae0e614c5d0f99f47f317
Pål Brattberg <pal@subtree.se>
2020-01-01 19:30:12 +0100

    Updated ahead of reinstall

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ Dead simple script to setup my new Mac:
 
 ```shell
 curl -sL https://raw.githubusercontent.com/pal/mac-setup-script/master/setup.sh | sh
+curl -sL https://raw.githubusercontent.com/pal/mac-setup-script/master/defaults.sh | sh
 ```
--- setup.sh
+++ setup.sh
@@ -1,71 +1,86 @@
 #!/usr/bin/env bash
 
 use_fish=false
+use_bash=true
 
-brews=(
-  bash
-  clib
-  gpg
-  htop
-  httpie
-  iftop
-  ncdu
-  nmap
-  node
-  python
-  git
-  scala
-  sbt
-  heroku
-  boot2docker
-  findutils
-  gnu-sed
-  maven
-  ant
-  jenv
-  nvm
-  rbenv
-  ruby-build
-  jq
-  wget
-  elasticsearch
-  mackup
+# Install some stuff before others!
+important_casks=(
+  1password
+  alfred
+  dropbox
+  google-chrome
+  tunnelblick
+  visual-studio-code
 )
 
 casks=(
-  google-chrome
+  # evernote #MAS
+  # java
+  # steam #WIN
+  # telegram #MAS
   firefox
-  atom
-  lastpass
+  font-eb-garamond
+  font-input
+  font-open-sans
+  font-open-sans-condensed
+  font-source-code-pro
+  google-drive-file-stream
   slack
-  java
-  virtualbox
-  datagrip
-  textwrangler
-  mamp
   spotify
-  hipchat
-  skype
-  evernote
-  alfred
-  dropbox
-  google-drive
-  microsoft-office)
+  transmit
+  transmission
+)
+
+brews=(
+  awscli
+  fish
+  fzf
+  git
+  mackup
+  mas
+  node
+  openssl
+  python
+  python3
+  ruby
+  wget
+  yarn
+)
+
+mas=(
+  # 497799835 Xcode (11.1)
+  405580712 #StuffIt Expander (15.0.7)
+  406056744 #Evernote (7.13)
+  407963104 #Pixelmator (3.8.6)
+  409183694 #Keynote (9.2)
+  409201541 #Pages (8.2)
+  409203825 #Numbers (6.2)
+  441258766 #Magnet (2.4.4)
+  747648890 #Telegram (5.7)
+)
 
 gems=(
-  bundler
+  # bundler
 )
 
 npms=(
+  npx
+  serverless
+  standard
 )
 
-clibs=(
-  bpkg/bpkg
+vscode=(
+  bierner.github-markdown-preview
+  bierner.markdown-checkbox
+  bierner.markdown-emoji
+  bierner.markdown-preview-github-styles
+  bungcip.better-toml
+  chenxsan.vscode-standardjs
+  orta.vscode-jest
+  tyriar.sort-lines
 )
 
-bkpgs=(
-)
-
+git_email='pal@subtree.se'
 git_configs=(
   "branch.autoSetupRebase always"
   "color.ui auto"
@@ -77,82 +92,106 @@ git_configs=(
   "rebase.autostash true"
   "rerere.autoUpdate true"
   "rerere.enabled true"
+  "user.email ${git_email}"
   "user.name Pål Brattberg"
-  "user.email pal@subtree.se"
-)
-
-apms=(
-  atom-beautify
-  language-scala
-  minimap
 )
 
 fonts=(
-  font-source-code-pro
   font-asap
+  font-eb-garamond
+  font-fira-code
+  font-input
   font-open-sans
   font-open-sans-condensed
-  font-eb-garamond
+  font-source-code-pro
   font-trebuchet-ms
-  font-input)
-
-omfs=(
-  jacaetevha
-  osx
-  thefuck
 )
 
 ######################################## End of app list ########################################
 set +e
+set -x
 
-if test ! $(which brew); then
-  echo "Installing Xcode ..."
-  xcode-select --install
-
-  echo "Installing Homebrew ..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-  echo "Updating Homebrew ..."
-  brew update
-  brew upgrade
-fi
-
-brew doctor
-brew tap homebrew/dupes
-
-fails=()
-
-function print_red {
-  red='\x1B[0;31m'
-  NC='\x1B[0m' # no color
-  echo -e "${red}$1${NC}"
+function prompt {
+  if [[ -z "${CI}" ]]; then
+    read -p "Hit Enter to $1 ..."
+  fi
 }
 
 function install {
   cmd=$1
   shift
-  for pkg in $@;
+  for pkg in "$@";
   do
     exec="$cmd $pkg"
-    echo "Executing: $exec"
-    if $exec ; then
+    #prompt "Execute: $exec"
+    if ${exec} ; then
       echo "Installed $pkg"
     else
-      fails+=($pkg)
-      print_red "Failed to execute: $exec"
+      echo "Failed to execute: $exec"
+      if [[ ! -z "${CI}" ]]; then
+        exit 1
+      fi
     fi
   done
 }
 
-function proceed_prompt {
-  read -p "Proceed with installation? " -n 1 -r
-  if [[ $REPLY =~ ^[Nn]$ ]]
-  then
-    exit 1
+function brew_install_or_upgrade {
+  if brew ls --versions "$1" >/dev/null; then
+    if (brew outdated | grep "$1" > /dev/null); then 
+      echo "Upgrading already installed package $1 ..."
+      brew upgrade "$1"
+    else 
+      echo "Latest $1 is already installed"
+    fi
+  else
+    brew install "$1"
   fi
 }
 
-# Disable fish for now
+if [[ -z "${CI}" ]]; then
+  sudo -v # Ask for the administrator password upfront
+  # Keep-alive: update existing `sudo` time stamp until script has finished
+  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+fi
+
+if test ! "$(command -v brew)"; then
+  prompt "Install Homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  if [[ -z "${CI}" ]]; then
+    prompt "Update Homebrew"
+    brew update
+    brew upgrade
+    brew doctor
+  fi
+fi
+export HOMEBREW_NO_AUTO_UPDATE=1
+
+echo "Install important software ..."
+brew tap caskroom/versions
+install 'brew cask install' "${important_casks[@]}"
+
+prompt "Install packages"
+install 'brew_install_or_upgrade' "${brews[@]}"
+brew link --overwrite ruby
+
+prompt "Set git defaults"
+for config in "${git_configs[@]}"
+do
+  git config --global ${config}
+done
+
+if [ "$use_bash" = true ]
+then
+  prompt "Upgrade bash"
+  brew install bash bash-completion2 fzf
+  sudo bash -c "echo $(brew --prefix)/bin/bash >> /private/etc/shells"
+  #sudo chsh -s "$(brew --prefix)"/bin/bash
+  # Install https://github.com/twolfson/sexy-bash-prompt
+  touch ~/.bash_profile #see https://github.com/twolfson/sexy-bash-prompt/issues/51
+  (cd /tmp && git clone --depth 1 --config core.autocrlf=false https://github.com/twolfson/sexy-bash-prompt && cd sexy-bash-prompt && make install) && source ~/.bashrc
+fi
+
 if [ "$use_fish" = true ]
 then
   echo "Setting up fish shell ..."
@@ -170,69 +209,32 @@ then
   ruby -v
 fi
 
-brew info ${brews[@]}
-proceed_prompt
-install 'brew install' ${brews[@]}
+echo "
+alias del='mv -t ~/.Trash/'
+alias ls='exa -l'
+alias cat=bat
+export EDITOR=code
+" >> ~/.bash_profile
 
-echo "Tapping casks ..."
+prompt "Install software"
+install 'brew cask install' "${casks[@]}"
+
+prompt "Install secondary packages"
+# install 'gem install' "${gems[@]}"
+install 'npm install --global' "${npms[@]}"
+install 'code --install-extension' "${vscode[@]}"
 brew tap caskroom/fonts
-brew tap caskroom/versions
+install 'brew cask install' "${fonts[@]}"
+install 'mas install' "${mas[@]}"
 
-brew cask info ${casks[@]}
-proceed_prompt
-install 'brew cask install --appdir="/Applications"' ${casks[@]}
-
-# TODO: add info part of install or do reinstall?
-#install 'pip install --upgrade' ${pips[@]}
-install 'sudo gem install' ${gems[@]}
-#install 'clib install' ${clibs[@]}
-#install 'bpkg install' ${bpkgs[@]}
-install 'npm install --global' ${npms[@]}
-#install 'apm install' ${apms[@]}
-install 'brew cask install' ${fonts[@]}
-
-echo "Adding homeshick ..."
-git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-source "$HOME/.homesick/repos/homeshick/homeshick.sh"
-homeshick clone pal/profile
-ln -s $HOME/.profile.d/init $HOME/.profile
-
-echo "Upgrading bash ..."
-sudo bash -c "echo $(brew --prefix)/bin/bash >> /private/etc/shells"
-
-echo "Setting git defaults ..."
-for config in "${git_configs[@]}"
-do
-  git config --global ${config}
-done
-#git alias rpush '! git up && git push'
-
-if [ "$use_fish" = true ]
-then
-  echo "Setting up go ..."
-  mkdir -p /usr/libs/go
-  echo "export GOPATH=/usr/libs/go" >> ~/.config/fish/config.fish
-  echo "export PATH=$PATH:$GOPATH/bin" >> ~/.config/fish/config.fish
+if [[ -z "${CI}" ]]; then
+  prompt "Install software from App Store"
+  mas list
 fi
 
-echo "Upgrading ..."
-# pip install --upgrade setuptools
-# pip install --upgrade pip
-sudo gem update --system
-
-echo "Cleaning up ..."
+prompt "Cleanup"
 brew cleanup
 brew cask cleanup
-brew linkapps
 
-for fail in ${fails[@]}
-do
-  echo "Failed to install: $fail"
-done
-
-echo 'export PATH="/usr/local/sbin:$PATH"' >> ~/.bash_profile
-
-echo "Run `mackup restore` after DropBox has done syncing"
-
-#read -p "Hit enter to run [OSX for Hackers] script..." c
-#sh -c "$(curl -sL https://gist.githubusercontent.com/brandonb927/3195465/raw/osx-for-hackers.sh)"
+echo "Run [mackup restore] after DropBox has done syncing ..."
+echo "Done!"
