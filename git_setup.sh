8fe948a68a287c8ed20749d5254e67e9bd2ecfec
Pål <pal@subtree.se>
2025-06-16 15:44:38 +0200

    chore: Refactor Brewfile and git_setup.sh to use a function for cloning repositories and comment out unused taps

--- git_setup.sh
+++ git_setup.sh
@@ -9,26 +9,37 @@ ROOT=~/dev
 mkdir -p $ROOT
 cd $ROOT
 
+# Function to clone a repo if it doesn't exist
+clone_if_not_exists() {
+    local repo=$1
+    local dir=$2
+    if [ ! -d "$dir" ]; then
+        git clone $repo $dir
+    else
+        echo "Repository $dir already exists, skipping..."
+    fi
+}
+
 # Peasy & related repos
-git clone --single-branch --branch master git@github.com:pal/peasy.git peasy-master
-git clone --single-branch --branch planetscale git@github.com:pal/peasy.git peasy
-git clone git@github.com:pal/frankfurter.git
-git clone git@github.com:pal/peasy_client.git
-git clone git@github.com:pal/peasyv3.git
-git clone git@github.com:subtree/peasy-ui.git
+clone_if_not_exists "git@github.com:pal/peasy.git --single-branch --branch master" "peasy-master"
+clone_if_not_exists "git@github.com:pal/peasy.git --single-branch --branch planetscale" "peasy"
+clone_if_not_exists "git@github.com:pal/frankfurter.git" "frankfurter"
+clone_if_not_exists "git@github.com:pal/peasy_client.git" "peasy_client"
+clone_if_not_exists "git@github.com:pal/peasyv3.git" "peasyv3"
+clone_if_not_exists "git@github.com:subtree/peasy-ui.git" "peasy-ui"
 
 # Subtree repos
-git clone git@github.com:subtree/saas-template.git
-git clone git@github.com:subtree/template-magic-board.git
-git clone git@github.com:subtree/setup-hosting.git
-git clone git@github.com:subtree/companynamemaker.com.git
-git clone git@github.com:subtree/juniormarketer.ai.git
-git clone git@github.com:subtree/social-image-creator.git
-git clone git@github.com:subtree/saas-template-upptime.git
-git clone git@github.com:subtree/subtree-sites.git
-git clone https://github.com/subtree/subtree.se.git
-git clone https://github.com/subtree/jujino.com.git
-git clone git@github.com:subtree/julafton.com.git
+clone_if_not_exists "git@github.com:subtree/saas-template.git" "saas-template"
+clone_if_not_exists "git@github.com:subtree/template-magic-board.git" "template-magic-board"
+clone_if_not_exists "git@github.com:subtree/setup-hosting.git" "setup-hosting"
+clone_if_not_exists "git@github.com:subtree/companynamemaker.com.git" "companynamemaker.com"
+clone_if_not_exists "git@github.com:subtree/juniormarketer.ai.git" "juniormarketer.ai"
+clone_if_not_exists "git@github.com:subtree/social-image-creator.git" "social-image-creator"
+clone_if_not_exists "git@github.com:subtree/saas-template-upptime.git" "saas-template-upptime"
+clone_if_not_exists "git@github.com:subtree/subtree-sites.git" "subtree-sites"
+clone_if_not_exists "https://github.com/subtree/subtree.se.git" "subtree.se"
+clone_if_not_exists "https://github.com/subtree/jujino.com.git" "jujino.com"
+clone_if_not_exists "git@github.com:subtree/julafton.com.git" "julafton.com"
 
 # Personal projects
 # git clone git@github.com:exchangeratesapi/exchangeratesapi.git
@@ -40,28 +51,34 @@ git clone git@github.com:subtree/julafton.com.git
 # git clone git@github.com:pal/orders-in-mysql.git
 # git clone git@github.com:pal/playbook-template.git
 # git clone git@github.com:pal/simple-counter-server.git
-git clone git@github.com:pal/mac-setup-script.git
-git clone git@github.com:pal/palbrattberg.com.git
-git clone git@github.com:pal/ai-pres.git
-git clone git@github.com:pal/deep-research.git
-git clone https://github.com/pal/domainchecker.git
-git clone https://github.com/pal/mousegame.git
+clone_if_not_exists "git@github.com:pal/mac-setup-script.git" "mac-setup-script"
+clone_if_not_exists "git@github.com:pal/palbrattberg.com.git" "palbrattberg.com"
+clone_if_not_exists "git@github.com:pal/ai-pres.git" "ai-pres"
+clone_if_not_exists "git@github.com:pal/deep-research.git" "deep-research"
+clone_if_not_exists "https://github.com/pal/domainchecker.git" "domainchecker"
+clone_if_not_exists "https://github.com/pal/mousegame.git" "mousegame"
 
 # Infrastructure & Tools
-git clone https://github.com/subtree/k8s-hosting.git
-git clone git@github.com:typemill/typemill.git
-git clone git@github.com:requarks/wiki.git
-git clone git@github.com:stackblitz-labs/bolt.diy.git
-git clone git@github.com:toolbeam/opencontrol.git
+clone_if_not_exists "https://github.com/subtree/k8s-hosting.git" "k8s-hosting"
+clone_if_not_exists "git@github.com:typemill/typemill.git" "typemill"
+clone_if_not_exists "git@github.com:requarks/wiki.git" "wiki"
+clone_if_not_exists "git@github.com:stackblitz-labs/bolt.diy.git" "bolt.diy"
+clone_if_not_exists "git@github.com:toolbeam/opencontrol.git" "opencontrol"
 
 # Other projects
-git clone git@github.com:WeDoProducts/productvoice.git
-git clone git@github.com:Shpigford/covid-containment.git
+clone_if_not_exists "git@github.com:WeDoProducts/productvoice.git" "productvoice"
+clone_if_not_exists "git@github.com:Shpigford/covid-containment.git" "covid-containment"
 
 # Setup Xcode if installed
 if [[ -d "/Applications/Xcode.app" ]]; then
-  sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
-  sudo xcodebuild -runFirstLaunch
+    # Only run xcode-select if it's not already set
+    if [[ $(xcode-select -p) != "/Applications/Xcode.app/Contents/Developer" ]]; then
+        sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+    fi
+    # Only run first launch if it hasn't been run before
+    if [[ ! -f "$HOME/Library/Developer/Xcode/FirstLaunch" ]]; then
+        sudo xcodebuild -runFirstLaunch
+    fi
 fi
 
 echo "All done, hack away! 👨‍💻"
